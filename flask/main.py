1775fa103f4f42a95ba0a794c7598ff49aa49468
dgerstel <dawidgerstel@gmail.com>
2021-01-31 19:45:56 +0100

    connect ML with Flask: make app work!

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 from wtforms import Form, TextAreaField, validators
 import os
+import joblib
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
+cur_dir = os.path.dirname(__file__)
+clf = joblib.load(os.path.join(cur_dir, '..', 'jupyter',
+                                    'cardio_model.pkl'))
+pipeline = joblib.load(os.path.join(cur_dir, '..', 'jupyter',
+                                    'pipeline.pkl'))
+
+def classify(params):
+    print("classify input shape:", params.shape)
+    label = {1: 'death', 0: 'survive'}
+    params = pd.DataFrame(params, columns=['time', 'serum_creatinine'])
+    X = pipeline.transform(params)
+    y = clf.predict(X)
+    return [label[score] for score in y]
 
 class InputDataForm(Form):
     param1 = TextAreaField('It has been how many days after the event'
@@ -40,10 +56,13 @@ def results():
     if request.method == 'POST' and form.validate():
         param1 = request.form['param1']
         param2 = request.form['param2']
-        return render_template('results.html', param1=param1,
-                               param2=param2)
+        X = np.array([[param1, param2]]).astype(float)
+        y = classify(X)
+        return render_template('results.html',
+                               params=(param1, param2), outcome=y)
     return render_template('input_data.html', form=form)
 if __name__ == '__main__':
     app.run(debug=True)
-
+    print('clf', clf)
+    print('pipe', pipeline)
 
