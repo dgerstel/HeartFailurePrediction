3d9bfcebe7c0ad08eac62dcfde6736bc7c4c5732
Dawid Gerstel <dawidgerstel@gmail.com>
2021-02-05 10:02:07 +0100

    Dynamically generate form with fields for all ML features

--- flask/main.py
+++ flask/main.py
@@ -1,17 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-from wtforms import Form, TextAreaField, validators
+from wtforms import Form, TextAreaField, validators, FormField
+from wtforms.fields import FieldList
+from flask_wtf import FlaskForm
+
+import jinja2
+env = jinja2.Environment()
+env.globals.update(zip=zip)
+
 import os
 import joblib
 import numpy as np
 import pandas as pd
 
+# App config
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "RandomString"
+app.jinja_env.globals.update(zip=zip) # enable zipping lists in templates
 
+# Global variables, input files
 cur_dir = os.path.dirname(__file__)
 clf = joblib.load(os.path.join(cur_dir, '..', 'jupyter',
                                     'cardio_model.pkl'))
 pipeline = joblib.load(os.path.join(cur_dir, '..', 'jupyter',
                                     'pipeline.pkl'))
+features = joblib.load(os.path.join(cur_dir, '..', 'jupyter',
+                                    'features.pkl'))
+print("FEATURES", features)
 
 def classify(params):
     print("classify input shape:", params.shape)
@@ -21,12 +35,20 @@ def classify(params):
     y = clf.predict(X)
     return [label[score] for score in y]
 
-class InputDataForm(Form):
-    param1 = TextAreaField('It has been how many days after the event'
-                           '("time" feature)?',
-                           [validators.DataRequired()])
-    param2 = TextAreaField('Serum creatinine [units]',
-                           [validators.DataRequired()])
+class OneParamForm(FlaskForm):
+    field = TextAreaField("Value")
+
+class InputDataForm(FlaskForm):
+    fields = FieldList(FormField(OneParamForm), min_entries=len(features))
+
+    #params = {f : TextAreaField(f"Please provide {f} value") for f in features}
+    #params = FieldList(TextAreaField(f"Provide next feature"))
+    # print("Params:", para)
+    # param1 = TextAreaField('It has been how many days after the event'
+    #                        '("time" feature)?',
+    #                        [validators.DataRequired()])
+    # param2 = TextAreaField('Serum creatinine [units]',
+    #                        [validators.DataRequired()])
 
     def validate(self):
         # TODO: implement validation (check if numerical)
@@ -45,7 +67,9 @@ class InputDataForm(Form):
 @app.route('/')
 def index():
     form = InputDataForm(request.form)
-    return render_template('input_data.html', form=form)
+    return render_template('input_data.html', form=form, labels=features)
+    #return render_template('input_data.html',
+    #                      form_features=zip(form, features))
 
 
 
